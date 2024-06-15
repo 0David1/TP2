fb09cc8264c01fbba805c6c063c0ea2ee456fd73
Juan Pablo <JuanPablo@JuanPB.local>
2024-06-15 15:22:38 -0300

    juan

--- src/main/java/aed/SistemaSIU.java
+++ src/main/java/aed/SistemaSIU.java
@@ -1,16 +1,19 @@
 package aed;
+import java.util.*;
 
 public class SistemaSIU {
 
 
     public class Trie<T>{
-        //funcion para devolver la definicion de una palabra
-        //funcion que devuelve el contenido del trie en una secuencia de strings.
         NodoTrie raiz;
 
         public Trie(){//constructor del trie
             raiz = new NodoTrie();
         }
+        public Trie(NodoTrie nuevaRaiz){//constructor para cuando quiero hacer recursion.
+            raiz = nuevaRaiz;
+        }
+
         public void definir(String palabra, T valor){//funcion define palabra en el trie
             int largoDePalabra = palabra.length();
             NodoTrie actual = raiz;
@@ -31,21 +34,30 @@ public class SistemaSIU {
             NodoTrie actual = raiz;
             for (int i = 0; i < largoDePalabra; i ++){
                 actual = actual.caracteres[(int)palabra.charAt(i)];
-                if (actual == null){
+                if (actual == null){//si la palabra no esta definida devuelve null.
                     return null;
                 }
-                // if (actual.caracteres[(int)palabra.charAt(i)] == null){
-                //     actual.caracteres[(int)palabra.charAt(i)] = new NodoTrie();
-                //     actual = actual.caracteres[(int)palabra.charAt(i)];
-                // }
-                // else {
-                //     actual = actual.caracteres[(int)palabra.charAt(i)];
-                // }
             }
-            return actual.definicion;
+            return actual.definicion;//actual.definicion puede ser null si el nodo no tiene definido nada, chequear en las funciones.
         }
 
+        public String[] listaDeStrings(){
+            ArrayList<String> resultado = new ArrayList<>();
+            NodoTrie actual = raiz;
 
+            // int largoDeCaracteres = actual.caracteres.length;
+            // for (int i = 0 ; i < largoDeCaracteres ; i ++){
+            //     if (actual.caracteres[i] != null){
+            //         StringBuffer palabra = new StringBuffer();
+            //         palabra.append((char) i);
+            //         if (actual.)
+            //     }
+            //     else{
+
+            //     }
+            // }
+            return resultado.toArray(new String[0]);
+        }
 
         public class NodoTrie{
             NodoTrie[] caracteres;
@@ -61,15 +73,69 @@ public class SistemaSIU {
 
 
 
-
     public class Materia {
         int [] CargoDocentes ;
-        // otro atributo para la lista enlazada de libretas universitarias
-        //otro atributo para los tries de donde se llega a esta materia
+        listaEnlazada<String> libretas;
+        listaEnlazada<TrieStringPar> referencias;//con esto guardo la refencia de los tries que apuntan a esta materia
         public Materia(){
 
+
         }
+        public class TrieStringPar{//cree un tipo de dato que relaciona el trie donde esta uno de los nombres de la materia, con el nombre que deberia estar.
+            String NombreDeLaMateria;
+            Trie<String> TrieDondeEsta;
+            public TrieStringPar(Trie<String> Materias, String nombreEspecifico){
+                NombreDeLaMateria = nombreEspecifico;
+                TrieDondeEsta = Materias;
+            }
+        }
+
+        public class listaEnlazada<T>{//aca guardo las libretas universitarias, y guardo las referencias junto el nombre de la materia.
+            private Nodo Primero;
+            private int size;
+            private class Nodo{
+                T libretaUniversitaria;
+                Nodo siguiente;
+                public Nodo(T alumno){
+                    libretaUniversitaria = alumno;
+                    siguiente = null;
+                }
+            }
+            public listaEnlazada(){
+                Primero = null;
+                size = 0;
+            }
+
+            public T desapilar(){
+                Nodo actual = Primero;
+                if (actual != null){
+                    Primero = actual.siguiente;
+                    return actual.libretaUniversitaria;
+                }
+                else{
+                    return null;
+                }
+            }
+
+            public int longitud (){
+                return size;
+            }
+            public void apilar(T alumno){
+                Nodo nuevo = new Nodo(alumno);
+                nuevo.siguiente = Primero;
+                Primero = nuevo;
+                size ++;
+            }
+        }
+
     }
+
+
+
+
+
+
+
 
 
     enum CargoDocente{
