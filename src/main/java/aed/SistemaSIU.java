faa39ce9fd08671cc5fbb9e80ce23cf0800a4394
Juan Pablo <JuanPablo@JuanPB.local>
2024-06-15 14:00:20 -0300

    juan

--- src/main/java/aed/SistemaSIU.java
+++ src/main/java/aed/SistemaSIU.java
@@ -2,6 +2,76 @@ package aed;
 
 public class SistemaSIU {
 
+
+    public class Trie<T>{
+        //funcion para devolver la definicion de una palabra
+        //funcion que devuelve el contenido del trie en una secuencia de strings.
+        NodoTrie raiz;
+
+        public Trie(){//constructor del trie
+            raiz = new NodoTrie();
+        }
+        public void definir(String palabra, T valor){//funcion define palabra en el trie
+            int largoDePalabra = palabra.length();
+            NodoTrie actual = raiz;
+            for (int i = 0; i < largoDePalabra; i ++){
+                if (actual.caracteres[(int)palabra.charAt(i)] == null){
+                    actual.caracteres[(int)palabra.charAt(i)] = new NodoTrie();
+                    actual = actual.caracteres[(int)palabra.charAt(i)];
+                }
+                else {
+                    actual = actual.caracteres[(int)palabra.charAt(i)];
+                }
+            }
+            actual.definicion = valor;
+        }   
+
+        public T definicion (String palabra){
+            int largoDePalabra = palabra.length();
+            NodoTrie actual = raiz;
+            for (int i = 0; i < largoDePalabra; i ++){
+                actual = actual.caracteres[(int)palabra.charAt(i)];
+                if (actual == null){
+                    return null;
+                }
+                // if (actual.caracteres[(int)palabra.charAt(i)] == null){
+                //     actual.caracteres[(int)palabra.charAt(i)] = new NodoTrie();
+                //     actual = actual.caracteres[(int)palabra.charAt(i)];
+                // }
+                // else {
+                //     actual = actual.caracteres[(int)palabra.charAt(i)];
+                // }
+            }
+            return actual.definicion;
+        }
+
+
+
+        public class NodoTrie{
+            NodoTrie[] caracteres;
+            T definicion;
+            public NodoTrie(){  
+                caracteres = new NodoTrie[256];
+                definicion = null;
+            }
+
+        }
+
+    }
+
+
+
+
+    public class Materia {
+        int [] CargoDocentes ;
+        // otro atributo para la lista enlazada de libretas universitarias
+        //otro atributo para los tries de donde se llega a esta materia
+        public Materia(){
+
+        }
+    }
+
+
     enum CargoDocente{
         AY2,
         AY1,
