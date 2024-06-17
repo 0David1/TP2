4465655fc3f77abec51bf97da77129e9273fea9b
Juan Pablo <JuanPablo@JuanPB.local>
2024-06-17 17:00:57 -0300

    juan

--- src/main/java/aed/SistemaSIU.java
+++ src/main/java/aed/SistemaSIU.java
@@ -9,8 +9,30 @@ public class SistemaSIU {
     }
 
     public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias){
-        throw new UnsupportedOperationException("Método no implementado aún");	    
+        Trie estudiantes = new Trie();
+        Trie<Trie<Materia>> carreras = new Trie();
+        int i = 0;
+        while(i < libretasUniversitarias.length){
+            estudiantes.definir(libretasUniversitarias[i],0); //agregamos a los estudiantes a su Trie
+            i++;
+        }
+        int j = 0;
+        while(j < infoMaterias.length){
+            int k = 0;
+            while(k < infoMaterias[j].getParesCarreraMateria().length){
+                String nombre_carrera = infoMaterias[j].getParesCarreraMateria()[k].getCarrera(); 
+                String nombre_materia = infoMaterias[j].getParesCarreraMateria()[k].getNombreMateria(); //creamos dos variables distintas para guardar los nombres de carrera y materia
+                if(carreras.definicion(nombre_carrera) == null){ //si no existe la carrera, la creamos con su Trie asociado a materias
+                    carreras.definir(nombre_carrera, new Trie<Materia>());
+                }
+                k ++;
+                carreras.definicion(nombre_carrera).definir(nombre_materia, new Materia()); //definimos la materia nueva
+            }
+            j++;
+        }
     }
+    
+    //Queda analizar la complejidad
 
     public void inscribir(String estudiante, String carrera, String materia){
         throw new UnsupportedOperationException("Método no implementado aún");
